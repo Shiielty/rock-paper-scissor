da1d06a7c58bcee34a3f2a77ec26965a79117de9
Shiielty <kazutonine@gmail.com>
2021-08-31 11:25:15 +0700

    Update script.js write the algorithm for the game

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log("Hello World!");
+
+//make a string variable called rock with initial value rock
+//make a string variable called paper with initial value paper
+//make a string variable called scissor with initial value scissor
+//make a number variable called roundNumber with initial value 1
+//make a number variable called computerScore with initial value 0
+//make a number variable called playerScore with initial value 0
+//make a new variable called computer Selection;
+
+//[computerPlay function]:
+//computer choose rock, paper, or scissor randomly and put the result to computerSelection variable.
+//return computerSelection.
+
+//[playerPlay function]:
+//player asked to input their selection use window prompt
+//while the input didn't match with any rock-paper-scissor selection, show "Wrong input, please type either rock, paper or scissor" and ask them again.
+//put their selection to the playerSelection variable
+//return playerSelection
+
+//showScore();
+
+//[playRound function(playerSelection, computerSelection)
+//compare the value between computer and player selection:
+//If computer win, return "Computer win", add computerScore +1, and roundNumber +1, then show the score and round number
+//Else if player win, return "You win" and, add playerScore +1, and roundNumber +1, then show the score and round number
+//Else if computer and player tie, return "Tie" and add round # + 1 then show the score and round number
+//Else, maybe the player didn't input the right selection, return "Please type either rock, paper, or scissor" and keep the round # as it is.
+
+//the game:
+
+
+// after 5 round
+//If playerScore > computerScore,
+//  display "Congratulation, you beat the computers"
+//Else if player score < computerScore,
+//  Display "Haha I win this, human XD"
+
