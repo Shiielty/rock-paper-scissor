3b5989a2b16959ec1fee70416e126f7c130c1df8
Shiielty <kazutonine@gmail.com>
2022-01-14 21:43:58 +0700

    Feat: Add DOMmethod to display the score directly in the website

--- script.js
+++ script.js
@@ -1,80 +1,95 @@
-window.alert(
-  `Hey there! Let's play Rock-Paper-Scissor game with me!\nOpen the console in your browser to start the game!`
-);
-console.log(
-  `Hey there! Let's play Rock-Paper-Scissor game with me!\nType "game()" in this console and press enter to start the game!`
-);
-
 const selection = ["rock", "paper", "scissor"];
-let roundNumber = 0;
+let gameRound = 0;
 let computerScore = 0;
 let playerScore = 0;
 let computerSelection;
 let playerSelection;
+let gameResult;
 
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorButton = document.querySelector(".scissor");
+
+const resultBox = document.querySelector(".results");
+const resultInfo = document.createElement("div");
+
+// computer choose rock, paper, or scissor(selection) randomly and put the result to computerSelection variable.
 function computerPlay() {
-  //computer choose rock, paper, or scissor(selection) randomly and put the result to computerSelection variable.
   computerSelection = selection[Math.floor(Math.random() * selection.length)];
   return computerSelection;
 }
 
-function playerPlay() {
-  //ask the player to choose between rock, paper, or scissor
-  playerSelection = window
-    .prompt("Choose Rock, Paper, or Scissor!")
-    .toLowerCase();
-  return playerSelection;
-}
-
+// compare the value between computer and player selection and return the result:
 function playRound(computerSelection, playerSelection) {
-  //compare the value between computer and player selection and return the result:
   if (computerSelection == playerSelection) {
-    roundNumber++;
-    return `ROUND ${roundNumber}!!
-You choose ${playerSelection}, Computer choose ${computerSelection}
-Tie! Both player choose ${computerSelection}
-Your score = ${playerScore} || Computer score = ${computerScore}`;
+    gameRound++;
+    gameResult = "Tie!";
+    return;
   } else if (
     (computerSelection == "rock" && playerSelection == "scissor") ||
     (computerSelection == "paper" && playerSelection == "rock") ||
     (computerSelection == "scissor" && playerSelection == "paper")
   ) {
     computerScore++;
-    roundNumber++;
-    return `ROUND ${roundNumber}!!
-You choose ${playerSelection}, Computer choose ${computerSelection}
-You lose! ${computerSelection} beat ${playerSelection}
-Your score = ${playerScore} || Computer score = ${computerScore}`;
+    gameRound++;
+    gameResult = "You lose!";
+    return;
   } else if (
     (playerSelection == "rock" && computerSelection == "scissor") ||
     (playerSelection == "paper" && computerSelection == "rock") ||
     (playerSelection == "scissor" && computerSelection == "paper")
   ) {
     playerScore++;
-    roundNumber++;
-    return `ROUND ${roundNumber}!!
-You choose ${playerSelection}, Computer choose ${computerSelection}
-You win! ${playerSelection} beat ${computerSelection}
-Your score = ${playerScore} || Computer score = ${computerScore}`;
+    gameRound++;
+    gameResult = "You win!";
+    return;
+  }
+}
+
+rockButton.addEventListener("click", () => {
+  playRound(computerPlay(), (playerSelection = "rock"));
+
+  if (playerScore == 5 || computerScore == 5) {
+    resultInfo.innerHTML = `Game Over! <br /> 
+                            Your scores: ${playerScore} <br />
+                            Computer scores: ${computerScore}`;
+    gameRound = 0;
+    playerScore = 0;
+    computerScore = 0;
   } else {
-    return `Input error: please type either "rock", "paper", or "scissor" :)`;
+    resultInfo.innerHTML = `Round: ${gameRound} <br /> You choose ${playerSelection}, Computer choose ${computerSelection} <br /> ${gameResult} <br /> Your scores: ${playerScore} <br /> Computer scores: ${computerScore}`;
   }
-}
+  resultBox.appendChild(resultInfo);
+});
 
-function game() {
-  //play 5 round game in the console
-  while (roundNumber < 5) {
-    console.log(playRound(computerPlay(), playerPlay()));
+paperButton.addEventListener("click", () => {
+  const round = playRound(computerPlay(), (playerSelection = "paper"));
+
+  if (playerScore == 5 || computerScore == 5) {
+    resultInfo.innerHTML = `Game Over! <br /> 
+                            Your scores: ${playerScore} <br />
+                            Computer scores: ${computerScore}`;
+    gameRound = 0;
+    playerScore = 0;
+    computerScore = 0;
+  } else {
+    resultInfo.innerHTML = `Round: ${gameRound} <br /> You choose ${playerSelection}, Computer choose ${computerSelection} <br /> ${gameResult} <br /> Your scores: ${playerScore} <br /> Computer scores: ${computerScore}`;
   }
+  resultBox.appendChild(resultInfo);
+});
 
-  //show the final result to the player
-  window.alert(
-    `Game end! Your scores is:
-Win: ${playerScore}
-Lose: ${computerScore}
-Tie: ${5 - playerScore - computerScore}
-Thanks for playing with me! Type "game()" again in the console if you want to do a rematch.`
-  );
-  playerScore = 0;
-  computerScore = 0;
-}
+scissorButton.addEventListener("click", () => {
+  const round = playRound(computerPlay(), (playerSelection = "scissor"));
+
+  if (playerScore == 5 || computerScore == 5) {
+    resultInfo.innerHTML = `Game Over! <br /> 
+                            Your scores: ${playerScore} <br />
+                            Computer scores: ${computerScore}`;
+    gameRound = 0;
+    playerScore = 0;
+    computerScore = 0;
+  } else {
+    resultInfo.innerHTML = `Round: ${gameRound} <br /> You choose ${playerSelection}, Computer choose ${computerSelection} <br /> ${gameResult} <br /> Your scores: ${playerScore} <br /> Computer scores: ${computerScore}`;
+  }
+  resultBox.appendChild(resultInfo);
+});
